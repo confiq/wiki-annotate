e4b8339a6b79ad47422556c2d68496fb77e1cf9b
igork <confiq@users.noreply.github.com>
2021-03-14 23:27:04 +0100

    make PreLoad tag

--- frontend/src/components/layouts/Main.js
+++ frontend/src/components/layouts/Main.js
@@ -8,41 +8,46 @@ import {
 } from "semantic-ui-react";
 // import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
 
+
 const main = () => (
   <Container id="main">
     <Segment raised>
-      <Grid columns={2} relaxed="very" stackable>
-        <Grid.Column>
-          <Dimmer active inverted>
-            <Loader inverted>Loading users</Loader>
-          </Dimmer>
-          <Placeholder>
-            <Placeholder.Paragraph>
-              <Placeholder.Line />
-              <Placeholder.Line />
-              <Placeholder.Line />
-              <Placeholder.Line />
-            </Placeholder.Paragraph>
-          </Placeholder>
-        </Grid.Column>
-
-        <Grid.Column>
-          <Dimmer active inverted>
-            <Loader inverted>Loading data</Loader>
-          </Dimmer>
-
-          <Placeholder>
-            <Placeholder.Paragraph>
-              <Placeholder.Line />
-              <Placeholder.Line />
-              <Placeholder.Line />
-              <Placeholder.Line />
-            </Placeholder.Paragraph>
-          </Placeholder>
-        </Grid.Column>
-      </Grid>
+      <PreLoad />
     </Segment>
   </Container>
 );
+
+
+const PreLoad = () => 
+<Grid columns={2} relaxed="very" stackable>
+<Grid.Column>
+  <Dimmer active inverted>
+    <Loader inverted>Loading users</Loader>
+  </Dimmer>
+  <Placeholder>
+    <Placeholder.Paragraph>
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Paragraph>
+  </Placeholder>
+</Grid.Column>
+
+<Grid.Column>
+  <Dimmer active inverted>
+    <Loader inverted>Loading data</Loader>
+  </Dimmer>
+
+  <Placeholder>
+    <Placeholder.Paragraph>
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+      <Placeholder.Line />
+    </Placeholder.Paragraph>
+  </Placeholder>
+</Grid.Column>
+</Grid>
 
 export default main;
