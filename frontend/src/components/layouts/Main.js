0e03c24133833e89c8316371c96801b09c64fbfb
igork <confiq@users.noreply.github.com>
2021-03-16 14:29:46 +0100

    first stable UI

--- frontend/src/components/layouts/Main.js
+++ frontend/src/components/layouts/Main.js
@@ -1,80 +1,43 @@
 import {
   Container,
   Segment,
-  Dimmer,
-  Loader,
-  Grid,
-  Placeholder,
   Table,
   Label,
   Icon,
+  Button,
 } from "semantic-ui-react";
-// import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
 
 const main = () => (
   <Container id="main">
-    <Segment>
-      <Segment.Group horizontal>
-        <Segment>Left</Segment>
-        <Segment>Right</Segment>
+      <Segment.Group horizontal id="header-diff">
+        <Segment textAlign="left">
+        <Icon loading name="spinner" /><Icon name="file" />
+        <b>{"page_name"}</b>
+        </Segment>
+        <Segment textAlign="right">
+          <Button>source</Button>
+          <Button disabled>rendered</Button><Label color="yellow" floating size="tiny">todo</Label>
+        </Segment>
       </Segment.Group>
       <PreLoad />
-    </Segment>
   </Container>
 );
 
 const PreLoad = () => (
   <Table celled fixed id="annotation">
-    <Table.Header>
-      <Table.Row>
-        <Table.HeaderCell colSpan="3">
-          {" "}
-          <Icon loading name="spinner" />
-          {"page_name"}
-        </Table.HeaderCell>
-      </Table.Row>
-    </Table.Header>
 
     <Table.Body>
       <Table.Row className="annotation_text">
-        <Table.Cell width="1">
+        <Table.Cell width="5">
           <Icon loading name="spinner" />
           {"user1, user2, user3"}
         </Table.Cell>
-        <Table.Cell>1</Table.Cell>
+        <Table.Cell width="1">1</Table.Cell>
         <Table.Cell>
           <Icon loading name="wait" />
           wiki_content
         </Table.Cell>
       </Table.Row>
-      {/* <Table.Row>
-        <Table.Cell>
-          <Icon name='folder' /> test
-        </Table.Cell>
-        <Table.Cell>Initial commit</Table.Cell>
-        <Table.Cell textAlign='right'>10 hours ago</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Icon name='folder' /> build
-        </Table.Cell>
-        <Table.Cell>Initial commit</Table.Cell>
-        <Table.Cell textAlign='right'>10 hours ago</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Icon name='file outline' /> package.json
-        </Table.Cell>
-        <Table.Cell>Initial commit</Table.Cell>
-        <Table.Cell textAlign='right'>10 hours ago</Table.Cell>
-      </Table.Row>
-      <Table.Row>
-        <Table.Cell>
-          <Icon name='file outline' /> Gruntfile.js
-        </Table.Cell>
-        <Table.Cell>Initial commit</Table.Cell>
-        <Table.Cell textAlign='right'>10 hours ago</Table.Cell>
-      </Table.Row> */}
     </Table.Body>
   </Table>
 );
