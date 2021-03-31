7b480301afcfe6bd01becdd7f35c2f8a955ab8ec
igork <confiq@users.noreply.github.com>
2021-03-31 22:15:59 +0300

    add DNS zone

--- infra/__main__.py
+++ infra/__main__.py
@@ -1,4 +1,12 @@
 """A Python Pulumi program"""
-from gcp import enable_services
+from gcp import enable_services, create_dns_zones
+import pulumi_gcp as gcp
+import pulumi
+
 
 enable_services()
+create_dns_zones()
+
+
+# current = gcp.organizations.get_client_config()
+# pulumi.export("project", current.access_token)
--- infra/gcp.py
+++ infra/gcp.py
@@ -1,5 +1,9 @@
 import pulumi_gcp as gcp
 
 def enable_services():
-    gcp.projects.Service("enable DNS",
+    gcp.projects.Service("enableDNS",
+        disable_dependent_services=True,
         service="dns.googleapis.com")
+
+def create_dns_zones():
+    gcp.dns.ManagedZone("wiki-zone", description="Root of blame.wiki", dns_name="blame.wiki.")
