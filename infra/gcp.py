5162dc9e12f455f75a01f9d9d1a36611006bb8e4
igork <confiq@users.noreply.github.com>
2021-03-31 22:51:38 +0300

    enable gcp cloud run

--- infra/gcp.py
+++ infra/gcp.py
@@ -4,6 +4,10 @@ def enable_services():
     gcp.projects.Service("enableDNS",
         disable_dependent_services=True,
         service="dns.googleapis.com")
+        
+    gcp.projects.Service('Enable Cloud Run',
+        service='run.googleapis.com'
+    )
 
 def create_dns_zones():
     gcp.dns.ManagedZone("wiki-zone", description="Root of blame.wiki", dns_name="blame.wiki.")
