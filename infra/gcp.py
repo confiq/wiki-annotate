5311850588f7d7dcbc034a5584f29f1d9963c7e0
igork <confiq@users.noreply.github.com>
2021-04-01 10:14:09 +0300

    use loop

--- infra/gcp.py
+++ infra/gcp.py
@@ -1,13 +1,11 @@
 import pulumi_gcp as gcp
 
 def enable_services():
-    gcp.projects.Service("enableDNS",
-        disable_dependent_services=True,
-        service="dns.googleapis.com")
-        
-    gcp.projects.Service('Enable Cloud Run',
-        service='run.googleapis.com'
-    )
+    enabled_services = ['dns.googleapis.com','run.googleapis.com','domains.googleapis.com']
+
+    for service in enabled_services:
+        gcp.projects.Service(f"enable service {service.split('.')[0].upper()}",
+            service=service)
 
 def create_dns_zones():
     gcp.dns.ManagedZone("wiki-zone", description="Root of blame.wiki", dns_name="blame.wiki.")
