9fbe7704b2ccf8842ce8edc102203e703280d2d0
Igor <confiq@users.noreply.github.com>
2026-02-27 11:33:23 +0100

    chore: migrate frontend to Vite + React 18
    - Replace react-scripts with Vite 7 (87ms cold start)
    - Upgrade React 17 -> 18, use createRoot
    - Migrate App.js class component -> functional hooks
    - REACT_APP_* -> VITE_* env vars, process.env -> import.meta.env
    - Move index.html to root (Vite convention)
    - Remove unused deps: history, react-universal-component, web-vitals
    - Fix JSX in .js files via esbuild loader config
    - Vulns: 172 -> 14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Message } from 'semantic-ui-react';
 import Annotation from "./components/Annotation";
 import Header from './components/layouts/Header';
-import Nav from './components/layouts/Nav'
+import Nav from './components/layouts/Nav';
 
+export default function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [pageData, setPageData] = useState({});
 
-export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      pageData: {}
-    };
-  }
-
-  componentDidMount() {
-    // TODO: this should be function
-    const url = process.env.REACT_APP_API_URL
-    let wiki_url = window.location.href
-    if (process.env.NODE_ENV === 'development') {
-      wiki_url = wiki_url.replace('localhost:3000', process.env.REACT_APP_DEBUG_DOMAIN)
+  useEffect(() => {
+    const url = import.meta.env.VITE_API_URL;
+    let wiki_url = window.location.href;
+    if (import.meta.env.MODE === 'development') {
+      wiki_url = wiki_url.replace('localhost:3000', import.meta.env.VITE_DEBUG_DOMAIN);
     }
     fetch(`${url}/v1/page_info/?url=${wiki_url}`)
       .then(res => res.json())
       .then(
         (result) => {
-          this.setState({
-            isLoaded: true,
-            pageData: result
-          });
+          setIsLoaded(true);
+          setPageData(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          })
+          setIsLoaded(true);
+          setError(error);
         }
-      )
-    // console.log(this.state)
-    };
+      );
+  }, []);
 
-  render() {
-    return (
+  return (
     <Container fluid>
       <Header />
-       {/* when we use router, move continer to somewhere else */}
-       <Container id='container_annotation'>
-       <Message hidden>You can't see me</Message>
-        <Nav pageName={this.state.pageData.page_title} isLoaded={this.state.isLoaded} />        
-        <Annotation parentState={this.state} />
+      <Container id='container_annotation'>
+        <Message hidden>You can't see me</Message>
+        <Nav pageName={pageData.page_title} isLoaded={isLoaded} />
+        <Annotation error={error} isLoaded={isLoaded} pageData={pageData} />
       </Container>
       <Container>
-      <div style={{ textAlign: "center"}}>
-        <p>Made with ❤️ and 😓. See how you can help: <a href="https://github.com/confiq/wiki-annotate">wiki-annotate</a> </p>
-      </div>
+        <div style={{ textAlign: "center" }}>
+          <p>Made with ❤️ and 😓. See how you can help: <a href="https://github.com/confiq/wiki-annotate">wiki-annotate</a></p>
+        </div>
       </Container>
     </Container>
-
-    );
-  }
+  );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,8 @@
-//inspired from https://stackblitz.com/edit/react-vrtngq
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
+import { createRoot } from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './style.css';
-import App from './App'
-const history = require("history").createBrowserHistory
+import App from './App';
 
-const render = App =>
-  ReactDOM.render(
-    <App history={history}/>
-    ,
-    document.getElementById('root')
-  )
-
-render(App)
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
